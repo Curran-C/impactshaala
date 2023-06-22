fb0cc3c340595417e7940b61716ac6e552832ad1
= <=>
2023-06-22 11:23:59 +0530

    sign up page added

--- src/pages/location/Location.jsx
+++ src/pages/location/Location.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import "./location.scss";
 
 const Location = () => {
-  return <div>Location</div>;
+  const handleSubmit = () => {};
+
+  return (
+    <div className="location">
+      <div className="signup white">
+        <div className="upload">
+          <span>Location</span>
+          <p>Lorem ipsum dolor sit amet consectetur</p>
+        </div>
+      </div>
+
+      <form className="signup" onSubmit={handleSubmit}>
+        <h1>Address</h1>
+
+        <div className="inputs">
+          <input placeholder="Address 1" type="text" name="" id="" />
+          <input placeholder="Address 2" type="email" name="" id="" />
+          <div className="pin">
+            <input placeholder="Pincode" type="text" />
+            <input placeholder="City" type="text" />
+          </div>
+          <input type="text" placeholder="State" />
+        </div>
+
+        <button type="submit">Next</button>
+      </form>
+    </div>
+  );
 };
 
 export default Location;
